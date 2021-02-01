d34b0c2d6458bee256793613076354d5b26fdc08
bgdnvk <bnovykov@gmail.com>
2021-02-01 20:12:56 +0100

    b4 middleware

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ app.get('/info', (req, res) => {
     ${new Date()}`)
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

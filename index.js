0d2c6e83e420dc5a09ce1a7f3c3a47fd1f6b11a9
bgdnvk <bnovykov@gmail.com>
2021-02-02 02:46:25 +0100

    morgan middleware

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postToken'))
+
+morgan.token('postToken', (req, res, param) => {
+    if(req.method === 'POST'){
+        return JSON.stringify(req.body)
+    }
+})
 
 let persons = [
     { id:1, name: 'Arto Hellas', number: '040-123456' },
@@ -11,6 +20,7 @@ let persons = [
 ]
 
 app.get('/api/persons', (req, res) => {
+    // console.log(req);
     res.json(persons)
 })
 
